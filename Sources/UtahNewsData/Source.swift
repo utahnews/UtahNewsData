91a8eaa997e3ed912eb57b597f404cfecc33f1f8
Mark Evans <email@markevans.dev>
2024-12-10 10:18:31 -0700

    cleanup spacing

--- Sources/UtahNewsData/Source.swift
+++ Sources/UtahNewsData/Source.swift
@@ -27,7 +27,11 @@ public struct Source: AssociatedData, Codable, Identifiable { // Adding Identifi
     public var JSONSchema: JSONSchema?                  // Matches NewsSource
 
     // If needed, a custom initializer to create a Source from a NewsSource instance:
-    init(newsSource: NewsSource, credibilityRating: Int? = nil, relationships: [Relationship] = []) {
+    init(
+        newsSource: NewsSource,
+        credibilityRating: Int? = nil,
+        relationships: [Relationship] = []
+    ) {
         self.id = newsSource.id
         self.name = newsSource.name
         self.url = newsSource.url
