da014c4e841d462ea99a8ad8d858a3e8afe7baf4
Mark Evans <email@markevans.dev>
2025-01-28 16:35:07 -0700

    Added UserSubmission object to the list of UtahNewsData objects

---
+++ Sources/UtahNewsData/UserSubmission.swift
@@ -0,0 +1,49 @@
+//
+//  File.swift
+//  UtahNewsData
+//
+//  Created by Mark Evans on 1/28/25.
+//
+
+import Foundation
+
+
+public struct UserSubmission: AssociatedData {
+    public var id: String
+    public var relationships: [Relationship] = []
+    public var title: String
+    public var description: String
+    public var dateSubmitted: Date
+    public var user: Person
+    public var text: [TextMedia]
+    public var images: [ImageMedia]
+    public var videos: [VideoMedia]
+    public var audio: [AudioMedia]
+    public var documents: [LegalDocument]
+    
+    init(
+        id: String,
+        relationships: [Relationship],
+        title: String,
+        description: String = "",
+        dateSubmitted: Date = Date(),
+        user: Person,
+        text: [TextMedia] = [],
+        images: [ImageMedia] = [],
+        videos: [VideoMedia] = [],
+        audio: [AudioMedia] = [],
+        documents: [LegalDocument] = []
+    ) {
+        self.id = id
+        self.relationships = relationships
+        self.title = title
+        self.description = description
+        self.dateSubmitted = dateSubmitted
+        self.user = user
+        self.text = text
+        self.images = images
+        self.videos = videos
+        self.audio = audio
+        self.documents = documents
+    }
+}
