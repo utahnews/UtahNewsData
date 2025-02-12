981cc74fbb75cbc2fa874f498f8410a0a4fc971d
Mark Evans <email@markevans.dev>
2025-02-12 11:51:08 -0700

    Updated Fact object with additional default initializer

--- Sources/UtahNewsData/Fact.swift
+++ Sources/UtahNewsData/Fact.swift
@@ -1,33 +1,54 @@
 //
 //  Fact.swift
-//  NewsCapture
+//  UtahNewsData
 //
 //  Created by Mark Evans on 10/25/24.
 //
 
 import SwiftUI
 
-
 public struct Fact: AssociatedData, Codable {
     public var id: String
     public var relationships: [Relationship] = []
     public var statement: String
-//    public var source: Source
     public var dateVerified: Date
 
+    // Default initializer
     public init(
         id: String = UUID().uuidString,
         statement: String,
-//        source: Source,
         dateVerified: Date
     ) {
         self.id = id
         self.statement = statement
-//        self.source = source
         self.dateVerified = dateVerified
     }
+    
+    // Custom initializer to supply a default id if missing.
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.statement = try container.decode(String.self, forKey: .statement)
+        self.dateVerified = try container.decode(Date.self, forKey: .dateVerified)
+        self.id = (try? container.decode(String.self, forKey: .id)) ?? UUID().uuidString
+        self.relationships = (try? container.decode([Relationship].self, forKey: .relationships)) ?? []
+    }
+    
+    // Standard encoding implementation.
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(relationships, forKey: .relationships)
+        try container.encode(statement, forKey: .statement)
+        try container.encode(dateVerified, forKey: .dateVerified)
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case relationships
+        case statement
+        case dateVerified
+    }
 }
-
 
 public enum Verification: String, CaseIterable {
     case none = "None"
