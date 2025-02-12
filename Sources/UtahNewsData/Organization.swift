5fa602c232cb425c4c4fbfc2b17ab426a06866e4
Mark Evans <email@markevans.dev>
2025-02-12 12:23:18 -0700

    updated Organization object

--- Sources/UtahNewsData/Organization.swift
+++ Sources/UtahNewsData/Organization.swift
@@ -11,20 +11,20 @@ public struct Organization: AssociatedData, Codable, Identifiable, Hashable {
     public var id: String
     public var relationships: [Relationship] = []
     public var name: String
-    public var description: String?
+    public var orgDescription: String?   // Renamed from "description"
     public var contactInfo: [ContactInfo]? = []
     public var website: String?
 
     public init(
         id: String = UUID().uuidString,
         name: String,
-        description: String? = nil,
+        orgDescription: String? = nil,
         contactInfo: [ContactInfo]? = nil,
         website: String? = nil
     ) {
         self.id = id
         self.name = name
-        self.description = description
+        self.orgDescription = orgDescription
         self.contactInfo = contactInfo
         self.website = website
     }
