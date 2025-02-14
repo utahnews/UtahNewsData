fe9ec2ae20d7cd9622ac97c34f9de4bc489c35b5
Mark Evans <email@markevans.dev>
2025-02-13 22:18:49 -0700

    Updated Location with Coordinate initializers

--- Sources/UtahNewsData/Location.swift
+++ Sources/UtahNewsData/Location.swift
@@ -5,6 +5,10 @@
 //  Created by Mark Evans on 10/25/24.
 //
 
+// Location.swift
+// Summary: Defines the Location structure for the UtahNewsData module.
+//          Now includes a convenience initializer to create a Location with coordinates.
+
 import SwiftUI
 
 public struct Location: AssociatedData, Codable, Hashable, Equatable {
@@ -13,10 +17,18 @@ public struct Location: AssociatedData, Codable, Hashable, Equatable {
     public var name: String
     public var address: String?
     public var coordinates: Coordinates?
-
+    
+    // Existing initializer
     public init(id: String = UUID().uuidString, name: String) {
         self.id = id
         self.name = name
+    }
+    
+    // New convenience initializer to include coordinates
+    public init(id: String = UUID().uuidString, name: String, coordinates: Coordinates?) {
+        self.id = id
+        self.name = name
+        self.coordinates = coordinates
     }
 }
 
